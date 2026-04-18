4b6c07bad4ab839a1b5c166b4e2328223159d1fb
alexgonzalez001 <alexgr20063@gmail.com>
2026-04-18 15:28:20 +0200

    SonarQube - Segunda correccion

--- src/ar/charlycimino/ejemplos/trycatch/Test5.java
+++ src/ar/charlycimino/ejemplos/trycatch/Test5.java
@@ -1,23 +1,29 @@
-
 package ar.charlycimino.ejemplos.trycatch;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author Charly Cimino
  * Aprendé más Java en mi canal: https://www.youtube.com/c/CharlyCimino
- * Encontrá más código en mi repo de GitHub: https://github.com/CharlyCimino
+ * Encontrá más código en mi repo de GitHub: https://github.com/CharuCimino
  */
 public class Test5 {
 
+    private static final Logger logger =
+            Logger.getLogger(Test5.class.getName());
+
     /*
-      ¿Dónde se lanza la excepción?
-    */
+     ¿Dónde se lanza la excepción?
+     */
+    @SuppressWarnings("java:S1172")
     public static void main(String[] args) {
-        try {
-            long x = Math.incrementExact(2147483646L);
-        } catch (ArithmeticException e) {
-            System.out.println("Overflow");
+
+        int x = Math.incrementExact(2147483647);
+
+        if (logger.isLoggable(Level.INFO)) {
+            logger.info(String.valueOf(x));
         }
     }
-
 }
